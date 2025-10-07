e24be6e9b22a1fba483c89798fd56b723d368c87
Yogesh Pal <yogeshpal5049@gmail.com>
2025-10-08 00:48:27 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,9 @@ import { TopNavBar } from "components/TopNavBar";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: "OpenResume - Free Open-source Resume Builder and Parser",
+  title: "Resume Parser",
   description:
-    "OpenResume is a free, open-source, and powerful resume builder that allows anyone to create a modern professional resume in 3 simple steps. For those who have an existing resume, OpenResume also provides a resume parser to help test and confirm its ATS readability.",
-};
+"Resume Parser"};
 
 export default function RootLayout({
   children,
